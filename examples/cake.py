176d4d3130bc20fc6a9e0680f20f92878da58c17
Giulio Bottari <giulio.bottari@tvglobo.com.br>
2020-02-18 11:12:06 -0300

    Fix a bug that used the wrong grammatical person in the cake example

--- examples/cake.py
+++ examples/cake.py
@@ -5,27 +5,27 @@ from aida import render, LangConfig, Lang, NP, GNumber, Gender, VP, GPerson
 subj = (NP('I')
         .push(Lang.ENGLISH, GNumber.SINGULAR)
         .add_mapping('I', GPerson.FIRST)
-        .add_mapping('he', GPerson.SECOND, Gender.MALE)
+        .add_mapping('he', GPerson.THIRD)
         .pop().push(GNumber.PLURAL)
         .add_mapping('we', GPerson.FIRST)
-        .add_mapping('they', GPerson.SECOND)
+        .add_mapping('they', GPerson.THIRD)
         .clear().push(Lang.PORTUGUESE, GNumber.SINGULAR)
         .add_mapping('eu', GPerson.FIRST)
-        .add_mapping('ele', GPerson.SECOND, Gender.MALE)
+        .add_mapping('ele', GPerson.THIRD)
         .pop().push(GNumber.PLURAL)
         .add_mapping('nós', GPerson.FIRST)
-        .add_mapping('eles', GPerson.SECOND))
+        .add_mapping('eles', GPerson.THIRD))
 
 verb = (VP('make')
-        .push(Lang.ENGLISH, GNumber.SINGULAR)
-        .add_mapping('make', GNumber.PLURAL, GPerson.FIRST)
-        .add_mapping('makes', GPerson.SECOND)
+        .push(Lang.ENGLISH)
+        .add_mapping('make', GNumber.SINGULAR, GNumber.PLURAL, GPerson.FIRST)
+        .add_mapping('makes', GNumber.SINGULAR, GPerson.THIRD)
         .clear().push(Lang.PORTUGUESE, GNumber.SINGULAR)
         .add_mapping('faço', GPerson.FIRST)
-        .add_mapping('faz', GPerson.SECOND)
+        .add_mapping('faz', GPerson.THIRD)
         .pop().push(GNumber.PLURAL)
         .add_mapping('fazemos', GPerson.FIRST)
-        .add_mapping('fazem', GPerson.SECOND))
+        .add_mapping('fazem', GPerson.THIRD))
 
 cake = (NP('a cake')
         .add_mapping('a cake', Lang.ENGLISH)
@@ -34,7 +34,7 @@ cake = (NP('a cake')
 node = (subj | verb | cake).sentence()
 conf = LangConfig(node)
 
-for lang, person, number in product((Lang.ENGLISH, Lang.PORTUGUESE), (GNumber.SINGULAR, GNumber.PLURAL), (GPerson.FIRST, GPerson.SECOND)):
+for lang, person, number in product((Lang.ENGLISH, Lang.PORTUGUESE), (GNumber.SINGULAR, GNumber.PLURAL), (GPerson.FIRST, GPerson.THIRD)):
     conf.lang = lang
     conf.person = person
     conf.number = number
